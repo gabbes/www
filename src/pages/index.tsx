1c9f021da16ece3a96fb0c79f9dbe6cba430bb2c
gabbes <gabtost@gmail.com>
2020-01-01 20:19:44 +0100

    feat(index): add vs marketplace link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,30 @@ import 'normalize.css';
 import './index.css';
 
 const links = [
-  { label: 'github', url: 'https://github.com/gabbes' },
-  { label: 'npm', url: 'https://www.npmjs.com/~gabbe' },
-  { label: 'twitter', url: 'https://twitter.com/g4bbe' },
-  { label: 'last.fm', url: 'https://www.last.fm/user/spunklemeow' },
-  { label: 'steam', url: 'https://steamcommunity.com/id/g4bbe/' }
+  {
+    label: 'github',
+    url: 'https://github.com/gabbes'
+  },
+  {
+    label: 'npm',
+    url: 'https://www.npmjs.com/~gabbe'
+  },
+  {
+    label: 'twitter',
+    url: 'https://twitter.com/g4bbe'
+  },
+  {
+    label: 'last.fm',
+    url: 'https://www.last.fm/user/spunklemeow'
+  },
+  {
+    label: 'steam',
+    url: 'https://steamcommunity.com/id/g4bbe/'
+  },
+  {
+    label: 'visual studio marketplace',
+    url: 'https://marketplace.visualstudio.com/publishers/gabrieltollstalbom'
+  }
 ];
 
 export default () => (
